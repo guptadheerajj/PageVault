b2ab172b66d0e89ad92c78ac83bf121a7adda9dc
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-03-14 10:48:21 +0530

    feat: made display logic

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
 const myLibrary = [];
 
-function Book(title, author, pages, genre, isRead) {
+function Book(title, author, pages, language, genre, isRead) {
+	if (!new.target) {
+		throw Error("Use new keyword to call a constructor function");
+	}
 	this.uid = crypto.randomUUID();
-	this.title = title;
-	this.author = author;
+	this.title = title.charAt(0).toUpperCase() + title.slice(1);
+	this.author = author.charAt(0).toUpperCase() + author.slice(1);
 	this.pages = pages;
-	this.genre = genre;
+	this.language = language.charAt(0).toUpperCase() + language.slice(1);
+	this.genre = genre.charAt(0).toUpperCase() + genre.slice(1);
 	this.isRead = isRead;
 }
 
-function addBookToLibrary(title, author, pages, genre, isRead) {
-	const book = new Book(title, author, pages, genre, isRead);
+function addBookToLibrary(title, author, pages, language, genre, isRead) {
+	const book = new Book(title, author, pages, language, genre, isRead);
 	myLibrary.push(book);
 }
 
-function display(myLibrary) {
+function createCard(book) {
+	const card = document.createElement("div");
+	card.classList.add("card");
+	const content = `<div class="title">${book.title}</div>
+			<div class="author info"><span class="info-type">By:</span> <span>${book.author}</span></div>
+			<div class="pages info"><span class="info-type">Number of Pages:</span> <span>${book.pages}</span></div>
+			<div class="language info"><span class="info-type">Language:</span> <span>${book.language}</span></div>
+			<div class="genre info"><span class="info-type">Genre:</span> <span>${book.genre}</span></div>
+			<label class="switch info">
+				<span class="info-type">Mark as read:</span>
+				<input type="checkbox">
+				<span class="slider"></span>
+			</label>`;
+	card.innerHTML = content;
+
+	return card;
+}
+
+function displayCards(myLibrary) {
+	const cardContainer = document.querySelector(".card-container");
 	for (const book of myLibrary) {
-		console.log(book);
+		const card = createCard(book);
+		cardContainer.appendChild(card);
 	}
 }
 
-addBookToLibrary("Operating system", "dheeraj", 21, "programming", "true");
-addBookToLibrary("Computer system", "noname", 26, "COmputer", "false");
-addBookToLibrary("Atomic system", "aryan", 24, "Chemistry", "false");
-addBookToLibrary("Video system", "akash", 20, "Design", "true");
+addBookToLibrary(
+	"Operating system",
+	"dheeraj",
+	21,
+	"english",
+	"programming",
+	"true"
+);
 
-display(myLibrary);
+addBookToLibrary("Atomic system", "aryan", 24, "english", "Chemistry", "false");
+addBookToLibrary("Video system", "akash", 20, "english", "Design", "true");
+
+displayCards(myLibrary);
